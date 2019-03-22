2395d5bce2f2f2c0f9593cb94eb50294e96877b0
Pavel Balobin <pavel@roamability.com>
2019-03-22 09:47:22 +0300

    190322

--- david_db_create.py
+++ david_db_create.py
@@ -5,10 +5,17 @@ import sqlite3
 conn = sqlite3.connect('david_db.sqlite') 
 cur = conn.cursor()
 cur.execute('DROP TABLE IF EXISTS CLIMATE_SENSORS')
+
 cur.execute('''CREATE TABLE CLIMATE_SENSORS (REP_DATE TEXT
             ,ID INTEGER PRIMARY KEY AUTOINCREMENT
             ,SENSOR_ID INTEGER
             ,ATTEMPT INTEGER
             ,TEMPERATURE NUMERIC
             ,HUMIDITY NUMERIC)''')
+
+cur.execute('DROP TABLE IF EXISTS SENSORS')
+
+cur.execute('''CREATE TABLE SENSORS (SENSOR_ID INTEGER
+            ,SENSOR_TYPE NVARCHAR(15)
+            ,LOCATION NVARCHAR(55)''')
 conn.commit()
--- david_web_server.py
+++ david_web_server.py
@@ -44,26 +44,27 @@ def check_file(file_name):
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-            self.get_climat_handler()
+            self.get_request_handler()
         finally:
             messagetosend = bytes('OK', 'utf')
             self.send_response(200)
             self.end_headers()
             self.wfile.write(messagetosend)
         
-    def get_climat_handler(self):
+    def get_request_handler(self):
         url = re.findall('GET /(.*) HTTP', self.requestline)
         get_url = urlparse(url[0])
         get_params = parse_qs(get_url.params, keep_blank_values=True)
+
         if get_url.path == 'climate':
             sensor_id = get_params.get('sensor')[0]
             attempt = get_params.get('readattempt')[0]
             temperature = get_params.get('temperature')[0]
             humidity = get_params.get('humidity')[0]
+            climate.info(f'Message=read_sensor;Sensor={sensor_id};Attempt={attempt};Temp={temperature};Hum={humidity}')
             try:
                 conn = sqlite3.connect(file_sqlite_db)
                 cur = conn.cursor()
-                climate.info(f'Message=read_sensor;Sensor={sensor_id};Attempt={attempt};Temp={temperature};Hum={humidity}')
                 cur.execute('''INSERT INTO CLIMATE_SENSORS (REP_DATE, SENSOR_ID, ATTEMPT, TEMPERATURE, HUMIDITY)
                                VALUES (datetime(), ?, ?, ?, ?)''', (sensor_id, attempt, temperature, humidity))
             except Exception as e:
@@ -80,7 +81,7 @@ class MyHandler(BaseHTTPRequestHandler):
 check_file(file_sqlite_db)
 check_file(file_log)
 
-      
+
 with HTTPServer((ip_addr, port), MyHandler) as httpd:
     print(f"Serving on port {port}...\nVisit http://{ip_addr}:{port}\nTo kill the server enter 'Ctrl + C'")
     httpd.serve_forever()
