d09ff392cc1e379a86222d1bf6b3fbcfe86ece80
Pavel Balobin <pavel@roamability.com>
2019-04-05 10:16:39 +0300

    190405

--- david_db_create.py
+++ david_db_create.py
@@ -2,20 +2,60 @@
 
 import sqlite3
 
-conn = sqlite3.connect('david_db.sqlite') 
-cur = conn.cursor()
-cur.execute('DROP TABLE IF EXISTS CLIMATE_SENSORS')
+# DavidServer
+file_sqlite_db = r'/home/user/david/david_db.sqlite'
 
-cur.execute('''CREATE TABLE CLIMATE_SENSORS (REP_DATE TEXT
+# For tests
+file_sqlite_db = r'c:\Users\balob\Downloads\DAVID\david_db.sqlite'
+
+conn = sqlite3.connect(file_sqlite_db)
+cur = conn.cursor()
+
+# cur.execute('DROP TABLE IF EXISTS CLIMATE_SENSORS')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS CLIMATE_SENSORS (REP_DATE TEXT
             ,ID INTEGER PRIMARY KEY AUTOINCREMENT
-            ,SENSOR_ID INTEGER
+            ,SENSOR_ID INTEGER NOT NULL
             ,ATTEMPT INTEGER
             ,TEMPERATURE NUMERIC
             ,HUMIDITY NUMERIC)''')
 
 cur.execute('DROP TABLE IF EXISTS SENSORS')
 
-cur.execute('''CREATE TABLE SENSORS (SENSOR_ID INTEGER
+cur.execute('''CREATE TABLE IF NOT EXISTS SENSORS (SENSOR_ID INTEGER PRIMARY KEY
             ,SENSOR_TYPE NVARCHAR(15)
-            ,LOCATION NVARCHAR(55)''')
+            ,LOCATION NVARCHAR(55))''')
+
+cur.execute('''INSERT INTO SENSORS (SENSOR_ID, SENSOR_TYPE, LOCATION) VALUES (?, ?, ?)''', (1, 'climate', 'bedroom'))
+cur.execute('''INSERT INTO SENSORS (SENSOR_ID, SENSOR_TYPE, LOCATION) VALUES (?, ?, ?)''', (3, 'motion', 'indoor'))
+
+#cur.execute('DROP TABLE IF EXISTS MOTION_SENSORS')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS MOTION_SENSORS (REP_DATE TEXT
+            ,ID INTEGER PRIMARY KEY AUTOINCREMENT
+            ,SENSOR_ID INTEGER NOT NULL)''')
+
+cur.execute('DROP VIEW IF EXISTS V_CLIMATE_SENSORS')
+
+cur.execute('''CREATE VIEW V_CLIMATE_SENSORS AS
+            SELECT
+            strftime('%Y-%m-%d %H:%M', datetime(cs.REP_DATE, 'localtime')) AS REP_DATE
+            ,s.LOCATION
+            ,cs.TEMPERATURE
+            ,cs.HUMIDITY
+            FROM CLIMATE_SENSORS cs, SENSORS s
+            WHERE cs.SENSOR_ID = s.SENSOR_ID
+            ORDER BY s.LOCATION, cs.REP_DATE DESC''')
+
+cur.execute('DROP VIEW IF EXISTS V_MOTION_SENSORS')
+
+cur.execute('''CREATE VIEW V_MOTION_SENSORS AS
+            SELECT
+            strftime('%Y-%m-%d %H:%M', datetime(ms.REP_DATE, 'localtime')) AS REP_DATE
+            ,s.LOCATION
+            FROM MOTION_SENSORS ms, SENSORS s
+            WHERE ms.SENSOR_ID = s.SENSOR_ID
+            ORDER BY s.LOCATION, ms.REP_DATE DESC''')
+
 conn.commit()
+conn.close()
--- david_web_server.py
+++ david_web_server.py
@@ -24,7 +24,7 @@ file_log = r'/home/user/david/log/david_web_server.log'
 
 # Create logger
 logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(asctime)s;Application=%(name)s;%(levelname)s;%(message)s')
-climate = logging.getLogger('climate')
+web_server_log = logging.getLogger('web_server')
 
 # Logger examples
 
@@ -36,9 +36,9 @@ climate = logging.getLogger('climate')
 
 def check_file(file_name):
     if isfile(file_name):
-        climate.info(f'Message=check_file;File={file_name};Result=exists')
+        web_server_log.info(f'Message=check_file;File={file_name};Result=exists')
     else:
-        climate.error(f'Message=check_file;File={file_name};Result=does_not_exist')
+        web_server_log.error(f'Message=check_file;File={file_name};Result=does_not_exist')
 
 
 class MyHandler(BaseHTTPRequestHandler):
@@ -61,26 +61,38 @@ class MyHandler(BaseHTTPRequestHandler):
             attempt = get_params.get('readattempt')[0]
             temperature = get_params.get('temperature')[0]
             humidity = get_params.get('humidity')[0]
-            climate.info(f'Message=read_sensor;Sensor={sensor_id};Attempt={attempt};Temp={temperature};Hum={humidity}')
+            web_server_log.debug(f'Message=read_sensor;Sensor={sensor_id};Attempt={attempt};Temp={temperature};Hum={humidity}')
             try:
                 conn = sqlite3.connect(file_sqlite_db)
                 cur = conn.cursor()
                 cur.execute('''INSERT INTO CLIMATE_SENSORS (REP_DATE, SENSOR_ID, ATTEMPT, TEMPERATURE, HUMIDITY)
                                VALUES (datetime(), ?, ?, ?, ?)''', (sensor_id, attempt, temperature, humidity))
             except Exception as e:
-                climate.error(f'Message=db_connect;Exception={e}')
+                web_server_log.error(f'Message=db_connect;Exception={e}')
             finally:
                 conn.commit()
                 conn.close()
         elif get_url.path == 'connected':
             sensor_id = get_params.get('sensor')[0]
             ip_addr = get_params.get('ip')[0]
-            climate.info(f'Message=connected;Sensor={sensor_id};IP={ip_addr}')
+            web_server_log.info(f'Message=connected;Sensor={sensor_id};IP={ip_addr}')
+        elif get_url.path == 'motion':
+            sensor_id = get_params.get('sensor')[0]
+            web_server_log.debug(f'Message=read_sensor;Sensor={sensor_id}')
+            try:
+                conn = sqlite3.connect(file_sqlite_db)
+                cur = conn.cursor()
+                cur.execute('''INSERT INTO MOTION_SENSORS (REP_DATE, SENSOR_ID)
+                               VALUES (datetime(), ?)''', (sensor_id))
+            except Exception as e:
+                web_server_log.error(f'Message=db_connect;Exception={e}')
+            finally:
+                conn.commit()
+                conn.close()
 
 
 check_file(file_sqlite_db)
 check_file(file_log)
-
 
 with HTTPServer((ip_addr, port), MyHandler) as httpd:
     print(f"Serving on port {port}...\nVisit http://{ip_addr}:{port}\nTo kill the server enter 'Ctrl + C'")
