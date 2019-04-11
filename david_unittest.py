a84f44caf7446ce1df5a4532741b9125da2ea80d
Pavel Balobin <pavel@roamability.com>
2019-04-11 09:41:54 +0300

    190411

--- david_unittest.py
+++ david_unittest.py
@@ -1,9 +1,22 @@
 import unittest
-from david_climate_check import file_sqlite_db
+import requests
 
-class ClimateCheckTest(unittest.TestCase):
+server_ip_addr = '192.168.1.52'
 
-    def test_check_file(self):
-        print(file_sqlite_db)
-        result = 5
-        self.assertEqual(result, result)
+class TestWebServer(unittest.TestCase):
+    
+    def test_get_connect(self):
+        url = f'http://{server_ip_addr}:80/connected;sensor=1&ip=192.168.1.63'
+        r = requests.get(url)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.text, 'OK')
+        
+    def test_get_climate(self):
+        url = f'http://{server_ip_addr}:80/climate;sensor=1&readattempt=10&temperature=24.0&humidity=35.0'
+        r = requests.get(url)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.text, 'OK')
+        
+suite = unittest.TestLoader().loadTestsFromTestCase(TestWebServer)
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(suite)
