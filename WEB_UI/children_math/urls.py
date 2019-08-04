92f1c3d479f5fb7299bfc8139255802537b8ad22
Pavel Balobin <pavel@roamability.com>
2019-08-04 10:23:31 +0300

    190704_math_task01_multiplication

--- WEB_UI/children_math/urls.py
+++ WEB_UI/children_math/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.children_math_main, name='children_math_main'),
 	path('children_math_01_task', views.children_math_01_task, name='children_math_01_task'),
-    path('children_math_01_result', views.children_math_01_result, name='children_math_01_result'),
+    # path('children_math_01_result', views.children_math_01_result, name='children_math_01_result'),
     path('children_math_02_task', views.children_math_02_task, name='children_math_02_task'),
     path('children_math_02_result', views.children_math_02_result, name='children_math_02_result'),
 ]
--- WEB_UI/children_math/views.py
+++ WEB_UI/children_math/views.py
@@ -52,55 +52,102 @@ def children_math_main(request):
 
 @login_required
 def children_math_01_task(request):
-    df = DataFrame(np.random.randint(1, 100, size=10).reshape((5, 2)), columns=['First', 'Second'])
-    df['Sign'] = np.random.choice(['-', '+'], 5)
-    list_feedback = []
-    for i in range(5):
-        list_feedback.append(f'<input class="form-control" id="feedback-Answer_{i}" type="text" name="Answer_{i}">')
-    df['Your Answer'] = list_feedback
-    df.index.name = '#'
-    df.reset_index(inplace=True)
-    df[['#', 'First', 'Sign', 'Second', 'Your Answer']].to_html(
-        f'children_math/templates/children_math/includes/{request.user.username}_math_01_task.html',
-        index=False, escape=False, justify='center', classes="table table-striped")
-    return render(request, 'children_math/children_math_01_task.html')
+    if request.method == 'GET':
+        df = DataFrame(np.random.randint(1, 30, size=10).reshape((5, 2)), columns=['First', 'Second'])
+        df['Sign'] = np.random.choice(['-', '+','*','/'], 5)
+        list_feedback = []
+        for i in range(5):
+            list_feedback.append(f'<input class="form-control" id="feedback-Answer_{i}" type="text" name="Answer_{i}">')
+        df['Your Answer'] = list_feedback
+        df.index.name = '#'
+        df.reset_index(inplace=True)
+        df[['#', 'First', 'Sign', 'Second', 'Your Answer']].to_html(
+            f'children_math/templates/children_math/includes/{request.user.username}_math_01_task.html',
+            index=False, escape=False, justify='center', classes="table table-striped")
+        return render(request, 'children_math/children_math_01_task.html')
+    elif request.method == 'POST':
+        Answer_0 = request.POST.get('Answer_0')
+        Answer_1 = request.POST.get('Answer_1')
+        Answer_2 = request.POST.get('Answer_2')
+        Answer_3 = request.POST.get('Answer_3')
+        Answer_4 = request.POST.get('Answer_4')
+        df_list = pd.read_html(
+            f'children_math/templates/children_math/includes/{request.user.username}_math_01_task.html', index_col=0)
+        df = df_list[0]
 
-@login_required
-def children_math_01_result(request):
-    Answer_0 = request.POST.get('Answer_0')
-    Answer_1 = request.POST.get('Answer_1')
-    Answer_2 = request.POST.get('Answer_2')
-    Answer_3 = request.POST.get('Answer_3')
-    Answer_4 = request.POST.get('Answer_4')
-    df_list = pd.read_html(
-        f'children_math/templates/children_math/includes/{request.user.username}_math_01_task.html', index_col=0)
-    df = df_list[0]
-    df['Correct Answer'] = df.apply(lambda x:
-                                    x['First'] + x['Second'] if x['Sign'] == '+' else x['First'] - x['Second'], axis=1)
-    df['Your Answer'] = [Answer_0, Answer_1, Answer_2, Answer_3, Answer_4]
-    df['Your Answer'] = df['Your Answer'].replace({'': 0}).astype('int')
+        def f(x):
+            if x['Sign'] == '+':
+                return x['First'] + x['Second']
+            elif x['Sign'] == '-':
+                return x['First'] - x['Second']
+            elif x['Sign'] == '*':
+                return x['First'] * x['Second']
+            elif x['Sign'] == '/':
+                return x['First'] / x['Second']
 
-    # for result in df[['First', 'Second', 'Sign', 'Your Answer']].values:
-    #     task_01 = Task01(user_id=request.user.id, first=result[0], second=result[1], sign=result[2], user_answer=result[3])
-    #     task_01.save()
+        df['Correct Answer'] = df.apply(f, axis=1)
 
-    objs = [
-        Task01(
-            user_id=request.user.id,
-            first=result[0],
-            second=result[1],
-            sign=result[2],
-            user_answer=result[3]
-        )
-        for result in df[['First', 'Second', 'Sign', 'Your Answer']].values
-    ]
-    Task01.objects.bulk_create(objs)
+        # df['Correct Answer'] = df.apply(lambda x:
+        #                                 x['First'] + x['Second'] if x['Sign'] == '+' else x['First'] - x['Second'],
+        #                                 axis=1)
+        df['Your Answer'] = [Answer_0, Answer_1, Answer_2, Answer_3, Answer_4]
+        df['Your Answer'] = df['Your Answer'].replace({'': 0}).astype('int')
 
-    df['Result'] = df.apply(lambda x: 'OK' if x['Your Answer'] == x['Correct Answer'] else 'WRONG', axis=1)
-    df.reset_index(inplace=True)
-    df.to_html('children_math/templates/children_math/includes/math_01_answer.html',
-               index=False, escape=False, justify='center', classes="table table-striped")
-    return render(request, 'children_math/children_math_01_result.html', {'score': len(df[df['Result'] == 'OK'])})
+        objs = [
+            Task01(
+                user_id=request.user.id,
+                first=result[0],
+                second=result[1],
+                sign=result[2],
+                user_answer=result[3]
+            )
+            for result in df[['First', 'Second', 'Sign', 'Your Answer']].values
+        ]
+        Task01.objects.bulk_create(objs)
+
+        df['Result'] = df.apply(lambda x: 'OK' if x['Your Answer'] == x['Correct Answer'] else 'WRONG', axis=1)
+        df.reset_index(inplace=True)
+        df[['#', 'First', 'Sign', 'Second', 'Your Answer','Result']]\
+            .to_html('children_math/templates/children_math/includes/math_01_answer.html',
+                   index=False, escape=False, justify='center', classes="table table-striped")
+        return render(request, 'children_math/children_math_01_result.html', {'score': len(df[df['Result'] == 'OK'])})
+
+# @login_required
+# def children_math_01_result(request):
+#     Answer_0 = request.POST.get('Answer_0')
+#     Answer_1 = request.POST.get('Answer_1')
+#     Answer_2 = request.POST.get('Answer_2')
+#     Answer_3 = request.POST.get('Answer_3')
+#     Answer_4 = request.POST.get('Answer_4')
+#     df_list = pd.read_html(
+#         f'children_math/templates/children_math/includes/{request.user.username}_math_01_task.html', index_col=0)
+#     df = df_list[0]
+#     df['Correct Answer'] = df.apply(lambda x:
+#                                     x['First'] + x['Second'] if x['Sign'] == '+' else x['First'] - x['Second'], axis=1)
+#     df['Your Answer'] = [Answer_0, Answer_1, Answer_2, Answer_3, Answer_4]
+#     df['Your Answer'] = df['Your Answer'].replace({'': 0}).astype('int')
+#
+#     # for result in df[['First', 'Second', 'Sign', 'Your Answer']].values:
+#     #     task_01 = Task01(user_id=request.user.id, first=result[0], second=result[1], sign=result[2], user_answer=result[3])
+#     #     task_01.save()
+#
+#     objs = [
+#         Task01(
+#             user_id=request.user.id,
+#             first=result[0],
+#             second=result[1],
+#             sign=result[2],
+#             user_answer=result[3]
+#         )
+#         for result in df[['First', 'Second', 'Sign', 'Your Answer']].values
+#     ]
+#     Task01.objects.bulk_create(objs)
+#
+#     df['Result'] = df.apply(lambda x: 'OK' if x['Your Answer'] == x['Correct Answer'] else 'WRONG', axis=1)
+#     df.reset_index(inplace=True)
+#     df.to_html('children_math/templates/children_math/includes/math_01_answer.html',
+#                index=False, escape=False, justify='center', classes="table table-striped")
+#     return render(request, 'children_math/children_math_01_result.html', {'score': len(df[df['Result'] == 'OK'])})
 
 @login_required
 def children_math_02_task(request):
