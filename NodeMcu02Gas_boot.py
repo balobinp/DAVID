11a5b27166249a25a0edb16241e0d88940813440
Pavel Balobin <pavel@roamability.com>
2020-11-08 10:04:41 +0300

    201108

--- NodeMcu02Gas_boot.py
+++ NodeMcu02Gas_boot.py
@@ -27,7 +27,7 @@ s_id_tmo_1 = 7 # temperature and motion in NodeMcu02Gas
 
 ### SET VARIABLES HERE ###
 
-version = 201018
+version = 201108
 
 ip_server = '192.168.1.44'
 # ip_server = '192.168.1.63'
